1233259a631ffa991c8a948a2dc45e6a99316f11
spennyp <hello@spencerperkins.me>
2024-06-29 12:12:49 -0700

    Added logging on event tracking, trying to debug how we are missing some events in Umami

--- web/src/utils/scroll.ts
+++ web/src/utils/scroll.ts
@@ -3,8 +3,6 @@ export function scrollToNounExplorer() {
   const destinationScrollY = element?.offsetTop;
   const currentScrollY = document.documentElement.scrollTop || document.body.scrollTop;
 
-  console.log("scrollToNounExplorer", { destinationScrollY, currentScrollY });
-
   // Slight delay to prevent scroll glitches as the page is resizing
   setTimeout(() => {
     window.scrollTo({
