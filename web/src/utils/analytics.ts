34309d34efe10675d7d177ff0b01e8654a354c70
spennyp <hello@spencerperkins.me>
2024-06-29 13:16:48 -0700

    Added plausible

--- web/src/utils/analytics.ts
+++ web/src/utils/analytics.ts
@@ -1,17 +1,36 @@
 import umami, { UmamiEventData } from "@umami/node";
+import Plausible from "plausible-tracker";
 
 umami.init({
   websiteId: process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID,
-  hostUrl: "https://analytics.paperclip.xyz",
+  hostUrl: "https://umami.paperclip.xyz",
+});
+
+const plausible = Plausible({
+  domain: process.env.NEXT_PUBLIC_PLAUSIBLE_DATA_DOMAIN,
 });
 
 export async function trackEvent(name: string, payload: UmamiEventData) {
   console.log("TRACKING EVENT", name, payload);
-  if (process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID) {
-    const resp = await umami.track(name, payload);
 
-    if (!resp.ok) {
-      console.error("Event tracking failed", resp.status, await resp.text());
+  // Umami
+  if (process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID) {
+    try {
+      const resp = await umami.track(name, payload);
+      if (!resp.ok) {
+        console.error("Event tracking failed", resp.status, await resp.text());
+      }
+    } catch (e) {
+      console.error("Umami event tracking failed", e);
+    }
+  }
+
+  // Plausible
+  if (process.env.NEXT_PUBLIC_PLAUSIBLE_DATA_DOMAIN) {
+    try {
+      plausible.trackEvent(name, payload);
+    } catch (e) {
+      console.error("Plausible event tracking failed", e);
     }
   }
 }
