2a86c92b65a16017c946639f4324b67ebaa57676
spennyp <hello@spencerperkins.me>
2024-06-29 01:22:27 -0700

    Enabled search bar hiding on mobile

--- web/src/components/NounExplorer/NounGrid/NounGrid.tsx
+++ web/src/components/NounExplorer/NounGrid/NounGrid.tsx
@@ -4,12 +4,7 @@ import { useMemo } from "react";
 import { Noun } from "@/data/noun/types";
 import AnimationGird from "./AnimationGrid";
 import { LinkShallow } from "../../ui/link";
-import { useSearchParams } from "next/navigation";
-import { isAddressEqual } from "viem";
-import { CHAIN_CONFIG } from "@/config";
 import { ClearAllFiltersButton } from "../NounFilter/ClearAllFiltersButton";
-import { ONLY_TREASURY_NOUNS_FILTER_KEY } from "../NounFilter/TreasuryNounFilter";
-import { INSTANT_SWAP_FILTER_KEY } from "../NounFilter/InstantSwapFilter";
 
 interface NounGridInterface {
   nouns: Noun[];
@@ -32,7 +27,7 @@ export default function NounGrid({ nouns }: NounGridInterface) {
   }, [nouns]);
 
   return (
-    <div className="relative min-h-[calc(100vh-108px)] w-full pb-[100px] md:min-h-[calc(100vh-64px)]">
+    <div className="relative w-full pb-[100px]">
       {nounCards.length == 0 ? (
         <div className="flex h-fit grow flex-col items-center justify-center gap-2 rounded-3xl border-4 border-gray-200 px-4 py-24 text-center">
           <h4>No Nouns found.</h4>
