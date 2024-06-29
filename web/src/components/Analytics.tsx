b0b603d83dc34d2f8123a627cc7f7fd27b83b642
spennyp <hello@spencerperkins.me>
2024-06-29 13:16:48 -0700

    Added plausible

---
+++ web/src/components/Analytics.tsx
@@ -0,0 +1,22 @@
+import Script from "next/script";
+
+export default function Analytics() {
+  return (
+    <>
+      {process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID && (
+        <Script
+          defer
+          src="https://umami.paperclip.xyz/script.js"
+          data-website-id={process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID}
+        />
+      )}
+      {process.env.NEXT_PUBLIC_PLAUSIBLE_DATA_DOMAIN && (
+        <Script
+          defer
+          src="https://plausible.paperclip.xyz/js/script.js"
+          data-domain={process.env.NEXT_PUBLIC_PLAUSIBLE_DATA_DOMAIN}
+        />
+      )}
+    </>
+  );
+}
