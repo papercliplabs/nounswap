5619db69164a359e7100113a892e2d496a7c4444
spennyp <hello@spencerperkins.me>
2024-06-29 12:12:49 -0700

    Added logging on event tracking, trying to debug how we are missing some events in Umami

--- web/src/utils/analytics.ts
+++ web/src/utils/analytics.ts
@@ -6,6 +6,7 @@ umami.init({
 });
 
 export async function trackEvent(name: string, payload: UmamiEventData) {
+  console.log("TRACKING EVENT", name, payload);
   if (process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID) {
     const resp = await umami.track(name, payload);
 
