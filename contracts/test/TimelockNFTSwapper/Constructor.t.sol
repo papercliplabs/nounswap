aeb3b51c18f1148fce2fc2b0b829a42aad3ab55a
spennyp <hello@spencerperkins.me>
2024-07-08 14:42:12 -0700

    Added TimelockNFTSwapper initital implementation and testing

---
+++ contracts/test/TimelockNFTSwapper/Constructor.t.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.20;
+
+import {Test, console} from "forge-std/Test.sol";
+import {TimelockNFTSwapper} from "../../src/TimelockNFTSwapper.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {MockERC721} from "../mock/MockERC721.sol";
+import {TimelockNFTSwapperTestHelper} from "./TimelockNFTSwapperTestHelper.sol";
+
+contract ConstructorTest is Test, TimelockNFTSwapperTestHelper {
+    ////
+    // Setup
+    ////
+
+    function setUp() public {
+        _basicSetup();
+    }
+
+    ////
+    // Successful Tests
+    ////
+
+    function test_constructor() public view {
+        assertEq(address(nftSwapper.NFT()), address(nft));
+        assertEq(nftSwapper.SWAP_POOL(), SWAP_POOL);
+        assertEq(nftSwapper.FEE_RECEIPIENT(), FEE_RECEIPIENT);
+        assertEq(nftSwapper.SWAP_PRICE_CURVE_BASE(), SWAP_PRICE_CURVE_BASE);
+        assertEq(nftSwapper.SWAP_PRICE_CURVE_SLOPE(), SWAP_PRICE_CURVE_SLOPE);
+        assertEq(nftSwapper.QUEUE_FEE(), QUEUE_FEE);
+        assertEq(nftSwapper.QUEUE_PERIOD(), QUEUE_PERIOD);
+        assertEq(nftSwapper.EXECUTION_GRACE_PERIOD(), EXECUTION_GRACE_PERIOD);
+    }
+}
