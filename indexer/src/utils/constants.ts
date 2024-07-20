240cb1b20d2e74c0875d6dc2849614e51ad2181b
spennyp <hello@spencerperkins.me>
2024-07-20 15:08:07 -0700

    Added ponder indexer, with initial data of treasury financials

---
+++ indexer/src/utils/constants.ts
@@ -0,0 +1,6 @@
+export const NOUNS_ERC20_DECIMALS = 18;
+export const NOUNS_ERC20_PER_NOUNS_NFT = 1_000_000n;
+
+export const MAINNET_SEC_PER_BLOCK = 12;
+
+export const SECONDS_PER_DAY = 24 * 60 * 60;
