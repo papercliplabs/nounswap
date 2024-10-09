0f114d8e8782b6e25d15dd8206e4173e96845d05
spennyp <hello@spencerperkins.me>
2024-10-09 00:05:07 -0700

    web: added Noun aquisition celebrations

--- web/src/components/SuccessDynamicLayout.tsx
+++ web/src/components/SuccessDynamicLayout.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { ReactNode } from "react";
 import ShareToFarcaster from "./ShareToFarcaster";
+import Confetti from "./Confetti";
 
 export interface SuccessDynamicLayoutProps {
   frameUrl: string;
@@ -18,23 +19,26 @@ export default function SuccessDynamicLayout({
   secondaryButton,
 }: SuccessDynamicLayoutProps) {
   return (
-    <div className="flex max-w-[900px] flex-col items-center justify-center gap-[40px] md:flex-row md:gap-[96px]">
-      <div className="aspect-square max-h-[400px] w-full max-w-[400px] rounded-[32px] p-[7px] shadow-2xl md:p-[12px]">
-        <Image src={`${frameUrl}/image`} alt="" width={400} height={400} className="h-full w-full rounded-[24px]" />
-      </div>
-      <div className="flex flex-col gap-8">
-        <div className="flex flex-col gap-2 pb-[120px] md:pb-0">
-          <span className="label-lg text-semantic-positive">Success!</span>
-          <div>
-            <h1>{title}</h1>
-            <div className="paragraph-lg">{subtitle}</div>
+    <>
+      <Confetti />
+      <div className="flex w-full max-w-[900px] flex-col items-center justify-center gap-[40px] md:flex-row md:gap-[96px]">
+        <div className="aspect-square max-h-[400px] w-full max-w-[400px] shrink-0 rounded-[32px] p-[7px] shadow-2xl md:w-[400px] md:p-[12px]">
+          <Image src={`${frameUrl}/image`} alt="" width={400} height={400} className="h-full w-full rounded-[24px]" />
+        </div>
+        <div className="flex flex-col gap-8">
+          <div className="flex flex-col gap-2 pb-[120px] md:pb-0">
+            <span className="label-lg text-semantic-positive">Success!</span>
+            <div>
+              <h1>{title}</h1>
+              <div className="paragraph-lg">{subtitle}</div>
+            </div>
+          </div>
+          <div className="border-border-secondary fixed bottom-0 left-0 flex w-screen flex-col gap-2 border-t-2 bg-white px-4 py-2 md:static md:w-auto md:border-none md:p-0">
+            <ShareToFarcaster text={socialShareCopy} embeds={[frameUrl]} />
+            {secondaryButton}
           </div>
         </div>
-        <div className="border-border-secondary fixed bottom-0 left-0 flex w-screen flex-col gap-2 border-t-2 bg-white px-4 py-2 md:static md:w-auto md:border-none md:p-0">
-          <ShareToFarcaster text={socialShareCopy} embeds={[frameUrl]} />
-          {secondaryButton}
-        </div>
       </div>
-    </div>
+    </>
   );
 }
