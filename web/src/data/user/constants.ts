15202976854155b718ed6303d8aa02b3fc5b5df8
spennyp <hello@spencerperkins.me>
2024-11-09 10:54:38 +0700

    web: consume dapp-kit identity api

--- web/src/data/user/constants.ts
+++ web/src/data/user/constants.ts
@@ -1,4 +1,5 @@
 import { CHAIN_CONFIG } from "@/config";
+import { IdentityConfig, IdentityResolver } from "@paperclip-labs/dapp-kit/identity/api";
 import { Address } from "viem";
 
 export const HARDCODED_USERS: Record<Address, { name: string; imageSrc: string }> = {
@@ -10,4 +11,11 @@ export const HARDCODED_USERS: Record<Address, { name: string; imageSrc: string }
     name: "$nouns ERC-20 contract",
     imageSrc: "/nouns-erc20.png",
   },
+};
+
+export const IDENTITY_RESOLVERS: IdentityResolver[] = ["nns", "ens", "farcaster"];
+
+export const IDENTITY_CONFIG: IdentityConfig = {
+  mainnetRpcUrl: `https://eth-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY!}`,
+  neynarApiKey: process.env.NEYNAR_API_KEY!,
 };
