8f879a9c53b6408213e7957adcf91675d57828c2
spennyp <hello@spencerperkins.me>
2025-01-09 23:12:43 -0800

    Moved dialogs to drawers on mobile

--- apps/web/src/components/dialog/NounSelectDialog.tsx
+++ apps/web/src/components/dialog/NounSelectDialog.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { Dialog, DialogContent } from "@/components/ui/dialogBase";
 import Icon from "../ui/Icon";
 import NounCard from "../NounCard";
 import { useEffect, useState } from "react";
@@ -12,6 +11,12 @@ import { useQuery } from "@tanstack/react-query";
 import { getNounsForAddress } from "@/data/noun/getNounsForAddress";
 import { Address } from "viem";
 import { useConnectModal } from "@rainbow-me/rainbowkit";
+import {
+  DrawerDialog,
+  DrawerDialogContent,
+  DrawerDialogTitle,
+} from "../ui/DrawerDialog";
+import LoadingSpinner from "../LoadingSpinner";
 
 interface NounSelectDialogProps {
   holderAddress?: Address;
@@ -44,24 +49,35 @@ export default function NounSelectDialog({
   }, [holderAddress, selectedNounCallback]);
 
   return (
-    <Dialog open={open} onOpenChange={(open) => setOpen(open)}>
+    <DrawerDialog open={open} onOpenChange={setOpen}>
       <>
         {selectedUserNoun ? (
           <div className="relative flex hover:cursor-pointer">
             <button onClick={() => setOpen(true)}>
-              <NounCard noun={selectedUserNoun} size={size ?? 200} enableHover={false} alwaysShowNumber />
+              <NounCard
+                noun={selectedUserNoun}
+                size={size ?? 200}
+                enableHover={false}
+                alwaysShowNumber
+              />
             </button>
             <button
               onClick={() => selectedNounCallback(undefined)}
               className="absolute right-0 top-0 -translate-y-1/2 translate-x-1/2 rounded-full bg-white"
             >
-              <Icon icon="circleX" size={40} className="rounded-full border-2 border-white fill-gray-600" />
+              <Icon
+                icon="circleX"
+                size={40}
+                className="rounded-full border-2 border-white fill-gray-600"
+              />
             </button>
           </div>
         ) : (
           <button
-            onClick={() => (holderAddress != undefined ? setOpen(true) : openConnectModal?.())}
-            className="bg-background-ternary text-content-secondary flex flex-col items-center justify-center gap-2 rounded-[20px] border-4 border-dashed p-8 hover:brightness-[85%]"
+            onClick={() =>
+              holderAddress != undefined ? setOpen(true) : openConnectModal?.()
+            }
+            className="flex flex-col items-center justify-center gap-2 rounded-[20px] border-4 border-dashed bg-background-ternary p-8 text-content-secondary hover:brightness-[85%]"
             style={{ width: size ?? 200, height: size ?? 200 }}
           >
             <Image src="/noggles.png" width={64} height={64} alt="" />
@@ -70,56 +86,60 @@ export default function NounSelectDialog({
         )}
       </>
 
-      <DialogContent className="flex max-h-[80vh] max-w-[425px] flex-col overflow-y-auto p-0">
-        <h4 className="px-6 pt-6">Select Noun</h4>
-        <div className="[&>ol>li>div]:text-content-secondary flex flex-col">
-          {userNouns == undefined ? (
-            <Icon icon="spinner" size={60} className="animate-spin" />
-          ) : userNouns.length == 0 ? (
-            <div className="flex h-[244px] w-full flex-col items-center justify-center gap-2 px-8 py-6 text-center">
-              <h4>No Nouns available</h4>
-              <div className="text-content-secondary">
-                {CHAIN_CONFIG.chain.id == 1 ? (
-                  <>
-                    Don{"'"}t have a noun on Ethereum? Try NounSwap on{" "}
-                    <LinkExternal
-                      className="text-semantic-accent hover:brightness-[85%]"
-                      href="https://sepolia.nounswap.wtf"
-                    >
-                      Sepolia Testnet
-                    </LinkExternal>
-                    .
-                  </>
-                ) : (
-                  <>
-                    You don{"'"}t have a noun on Testnet.
-                    <br />
-                    Buy a{" "}
-                    <Link href="/" className="text-semantic-accent">
-                      Testnet Noun here
-                    </Link>
-                    .
-                  </>
-                )}
+      <DrawerDialogContent className="md:max-h-[80vh] md:max-w-[425px]">
+        <div className="flex h-full min-h-0 w-full flex-col">
+          <DrawerDialogTitle className="w-full bg-white p-6 pb-2 shadow-fixed-bottom heading-4">
+            Select Noun
+          </DrawerDialogTitle>
+          <div className="justify-top z-0 flex h-full min-h-[400px] flex-col items-center overflow-y-scroll [&>ol>li>div]:text-content-secondary">
+            {userNouns == undefined ? (
+              <LoadingSpinner />
+            ) : userNouns.length == 0 ? (
+              <div className="flex h-[244px] w-full flex-col items-center justify-center gap-2 px-8 py-6 text-center">
+                <h4>No Nouns available</h4>
+                <div className="text-content-secondary">
+                  {CHAIN_CONFIG.chain.id == 1 ? (
+                    <>
+                      Don{"'"}t have a noun on Ethereum? Try NounSwap on{" "}
+                      <LinkExternal
+                        className="text-semantic-accent hover:brightness-[85%]"
+                        href="https://sepolia.nounswap.wtf"
+                      >
+                        Sepolia Testnet
+                      </LinkExternal>
+                      .
+                    </>
+                  ) : (
+                    <>
+                      You don{"'"}t have a noun on Testnet.
+                      <br />
+                      Buy a{" "}
+                      <Link href="/" className="text-semantic-accent">
+                        Testnet Noun here
+                      </Link>
+                      .
+                    </>
+                  )}
+                </div>
               </div>
-            </div>
-          ) : (
-            userNouns.map((noun, i) => (
-              <button
-                className="hover:bg-background-secondary flex w-full flex-row items-center gap-6 p-2 px-6 py-3 text-center hover:brightness-[85%]"
-                onClick={() => {
-                  selectedNounCallback(noun);
-                  setOpen(false);
-                }}
-                key={i}
-              >
-                <NounCard noun={noun} size={80} enableHover={false} />
-                <h4>Noun {noun.id}</h4>
-              </button>
-            ))
-          )}
+            ) : (
+              userNouns.map((noun, i) => (
+                <button
+                  className="flex w-full flex-row items-center gap-6 p-2 px-6 py-3 text-center hover:bg-background-secondary hover:brightness-[85%]"
+                  onClick={() => {
+                    selectedNounCallback(noun);
+                    setOpen(false);
+                  }}
+                  key={i}
+                >
+                  <NounCard noun={noun} size={80} enableHover={false} />
+                  <h4>Noun {noun.id}</h4>
+                </button>
+              ))
+            )}
+          </div>
         </div>
-      </DialogContent>
-    </Dialog>
+      </DrawerDialogContent>
+    </DrawerDialog>
   );
 }
--- apps/web/src/components/dialog/UserTipDialog.tsx
+++ apps/web/src/components/dialog/UserTipDialog.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { Dialog, DialogContent } from "@/components/ui/dialogBase";
 import Icon from "../ui/Icon";
 import { useEffect, useMemo, useState } from "react";
 import { Button } from "../ui/button";
@@ -14,15 +13,21 @@ import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
 import { useAccount, useBalance } from "wagmi";
 import { CHAIN_CONFIG } from "@/config";
 import { useConnectModal } from "@rainbow-me/rainbowkit";
+import { DrawerDialog, DrawerDialogContent } from "../ui/DrawerDialog";
 
 interface UserTipDialogProps {
   tip?: bigint;
   setTipCallback: (amount?: bigint) => void;
 }
 
-export default function UserTipDialog({ tip, setTipCallback }: UserTipDialogProps) {
+export default function UserTipDialog({
+  tip,
+  setTipCallback,
+}: UserTipDialogProps) {
   const [open, setOpen] = useState<boolean>(false);
-  const [formattedInputValue, setFormattedInputValue] = useState<string | undefined>(undefined);
+  const [formattedInputValue, setFormattedInputValue] = useState<
+    string | undefined
+  >(undefined);
 
   const { address } = useAccount();
 
@@ -49,28 +54,39 @@ export default function UserTipDialog({ tip, setTipCallback }: UserTipDialogProp
   }, [address, setTipCallback]);
 
   return (
-    <Dialog open={open} onOpenChange={(open) => setOpen(open)}>
+    <DrawerDialog open={open} onOpenChange={(open) => setOpen(open)}>
       <>
         {tip != undefined ? (
           <div className="relative hover:cursor-pointer">
             <button
               onClick={() => setOpen(true)}
-              className="bg-background-secondary flex h-[200px] w-[200px] flex-col items-center justify-center gap-4 rounded-3xl"
+              className="flex h-[200px] w-[200px] flex-col items-center justify-center gap-4 rounded-3xl bg-background-secondary"
             >
-              <Image src="/ethereum-logo.png" width={64} height={64} alt="WETH" />
+              <Image
+                src="/ethereum-logo.png"
+                width={64}
+                height={64}
+                alt="WETH"
+              />
               <h5>{formatTokenAmount(tip, NATIVE_ASSET_DECIMALS, 6)} WETH</h5>
             </button>
             <button
               onClick={() => setTipCallback(undefined)}
               className="absolute right-0 top-0 -translate-y-1/2 translate-x-1/2"
             >
-              <Icon icon="circleX" size={40} className="rounded-full border-4 border-white fill-gray-600" />
+              <Icon
+                icon="circleX"
+                size={40}
+                className="rounded-full border-4 border-white fill-gray-600"
+              />
             </button>
           </div>
         ) : (
           <button
-            onClick={() => (address != undefined ? setOpen(true) : openConnectModal?.())}
-            className="bg-background-ternary text-content-secondary flex h-[200px] w-[200px] flex-col items-center justify-center gap-2 rounded-[20px] border-4 border-dashed p-8 hover:brightness-[85%]"
+            onClick={() =>
+              address != undefined ? setOpen(true) : openConnectModal?.()
+            }
+            className="flex h-[200px] w-[200px] flex-col items-center justify-center gap-2 rounded-[20px] border-4 border-dashed bg-background-ternary p-8 text-content-secondary hover:brightness-[85%]"
           >
             <Image src="/tip.png" width={64} height={64} alt="" />
             <h6>Add a tip</h6>
@@ -78,95 +94,127 @@ export default function UserTipDialog({ tip, setTipCallback }: UserTipDialogProp
         )}
       </>
 
-      <DialogContent className="flex max-h-[80vh] max-w-[425px] flex-col overflow-y-auto">
-        <h4>Add a tip</h4>
-        <div className="flex flex-col gap-4">
-          <div>
-            Incentivize the DAO to accept the Swap Prop. <br />
-            You only pay this if the prop passes.
-          </div>
-          <div>
-            <div className="relative">
-              <Input
-                placeholder="0.00"
-                value={formattedInputValue}
-                className={twMerge("pr-24", insufficientBalance && "border-negative")}
-                onChange={(e) => {
-                  // Only positive decimal number inputs
-                  const regex = /^(0|0[.][0-9]*|[1-9][0-9]*[.]?[0-9]*)$/;
-                  if (e.target.value == "" || regex.test(e.target.value)) {
-                    setFormattedInputValue(e.target.value);
-                  }
-                }}
-              />
+      <DrawerDialogContent className="md:max-h-[80vh] md:max-w-[425px]">
+        <div className="flex flex-col overflow-y-auto p-6">
+          <h4>Add a tip</h4>
+          <div className="flex flex-col gap-4">
+            <div>
+              Incentivize the DAO to accept the Swap Prop. <br />
+              You only pay this if the prop passes.
+            </div>
+            <div>
+              <div className="relative">
+                <Input
+                  placeholder="0.00"
+                  value={formattedInputValue}
+                  className={twMerge(
+                    "pr-24",
+                    insufficientBalance && "border-negative",
+                  )}
+                  onChange={(e) => {
+                    // Only positive decimal number inputs
+                    const regex = /^(0|0[.][0-9]*|[1-9][0-9]*[.]?[0-9]*)$/;
+                    if (e.target.value == "" || regex.test(e.target.value)) {
+                      setFormattedInputValue(e.target.value);
+                    }
+                  }}
+                />
+                <div
+                  className={twMerge(
+                    "absolute right-5 top-1/2 flex h-full -translate-y-1/2 items-center border-l-2 pl-4 text-content-secondary",
+                    insufficientBalance && "border-negative",
+                  )}
+                >
+                  WETH
+                </div>
+              </div>
               <div
                 className={twMerge(
-                  "text-content-secondary absolute right-5 top-1/2 flex h-full -translate-y-1/2 items-center border-l-2 pl-4",
-                  insufficientBalance && "border-negative"
+                  "text-negative hidden",
+                  insufficientBalance && "flex",
                 )}
               >
-                WETH
+                Insufficient WETH balance.
               </div>
             </div>
-            <div className={twMerge("text-negative hidden", insufficientBalance && "flex")}>
-              Insufficient WETH balance.
+            <div className="text-content-secondary">
+              Balance:{" "}
+              <span className="font-bold">
+                {userBalance != undefined
+                  ? formatTokenAmount(
+                      userBalance?.value,
+                      NATIVE_ASSET_DECIMALS,
+                      6,
+                    )
+                  : "--"}{" "}
+                WETH{" "}
+                <button
+                  onClick={() =>
+                    userBalance != undefined
+                      ? setFormattedInputValue(formatEther(userBalance?.value))
+                      : {}
+                  }
+                  className="text-semantic-accent hover:text-semantic-accent-dark"
+                >
+                  (Max)
+                </button>
+              </span>
             </div>
-          </div>
-          <div className="text-content-secondary">
-            Balance:{" "}
-            <span className="font-bold">
-              {userBalance != undefined ? formatTokenAmount(userBalance?.value, NATIVE_ASSET_DECIMALS, 6) : "--"} WETH{" "}
-              <button
-                onClick={() =>
-                  userBalance != undefined ? setFormattedInputValue(formatEther(userBalance?.value)) : {}
-                }
-                className="text-semantic-accent hover:text-semantic-accent-dark"
-              >
-                (Max)
-              </button>
-            </span>
-          </div>
 
-          <div className="bg-semantic-accent-light flex flex-row items-center gap-3 rounded-xl p-4">
-            <Icon icon="circleQuestion" size={16} className="shrink-0" />
-            <div className="paragraph-sm">
-              Make sure you have enough WETH in your wallet when the prop executes.
-              <div className="flex flex-row justify-between pt-2">
-                <Tooltip>
-                  <TooltipTrigger className="text-content-secondary underline">
-                    <div>Why Wrapped ETH?</div>
-                  </TooltipTrigger>
-                  <TooltipContent className="text-content-primary flex max-w-[270px] flex-col gap-2 bg-white">
-                    <h6>Why WETH instead of ETH?</h6>
-                    <div>
-                      Using ETH as the tip would require sending it with the creation or execution transaction, and
-                      additional smart contract logic to ensure it is the correct amount for the proposal.
-                    </div>
-                    <div>
-                      In contrast, WETH can be pre-approved, then the transfer is executed and enforced as a transaction
-                      within the proposal.
-                    </div>
-                    <LinkExternal href={CHAIN_CONFIG.swapForWrappedNativeUrl} className="underline">
-                      Get Wrapped ETH
-                    </LinkExternal>
-                  </TooltipContent>
-                </Tooltip>
+            <div className="flex flex-row items-center gap-3 rounded-xl bg-semantic-accent-light p-4">
+              <Icon icon="circleQuestion" size={16} className="shrink-0" />
+              <div className="paragraph-sm">
+                Make sure you have enough WETH in your wallet when the prop
+                executes.
+                <div className="flex flex-row justify-between pt-2">
+                  <Tooltip>
+                    <TooltipTrigger className="text-content-secondary underline">
+                      <div>Why Wrapped ETH?</div>
+                    </TooltipTrigger>
+                    <TooltipContent className="flex max-w-[270px] flex-col gap-2 bg-white text-content-primary">
+                      <h6>Why WETH instead of ETH?</h6>
+                      <div>
+                        Using ETH as the tip would require sending it with the
+                        creation or execution transaction, and additional smart
+                        contract logic to ensure it is the correct amount for
+                        the proposal.
+                      </div>
+                      <div>
+                        In contrast, WETH can be pre-approved, then the transfer
+                        is executed and enforced as a transaction within the
+                        proposal.
+                      </div>
+                      <LinkExternal
+                        href={CHAIN_CONFIG.swapForWrappedNativeUrl}
+                        className="underline"
+                      >
+                        Get Wrapped ETH
+                      </LinkExternal>
+                    </TooltipContent>
+                  </Tooltip>
+                </div>
               </div>
             </div>
+            <Button
+              onClick={() => {
+                setTipCallback(
+                  formattedInputValue != undefined
+                    ? parseUnits(formattedInputValue, NATIVE_ASSET_DECIMALS)
+                    : undefined,
+                );
+                setOpen(false);
+              }}
+              disabled={
+                insufficientBalance ||
+                formattedInputValue == undefined ||
+                formattedInputValue == ""
+              }
+            >
+              Add tip
+            </Button>
           </div>
-          <Button
-            onClick={() => {
-              setTipCallback(
-                formattedInputValue != undefined ? parseUnits(formattedInputValue, NATIVE_ASSET_DECIMALS) : undefined
-              );
-              setOpen(false);
-            }}
-            disabled={insufficientBalance || formattedInputValue == undefined || formattedInputValue == ""}
-          >
-            Add tip
-          </Button>
         </div>
-      </DialogContent>
-    </Dialog>
+      </DrawerDialogContent>
+    </DrawerDialog>
   );
 }
